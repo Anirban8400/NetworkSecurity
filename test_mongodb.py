a6ef0c8c5aab41986e8e344f50c50ab04462a51c
Anirban Chatterjee <officialanirban2004@gmail.com>
2025-09-24 20:45:26 +0530

    fastapi done

--- test_mongodb.py
+++ test_mongodb.py
@@ -1,11 +1,12 @@
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import certifi
 
 uri = "mongodb+srv://chatterjeeanirban2004_db_user:Anirban2004@cluster0.xatnisb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
+client =MongoClient(uri, tls=True, tlsCAFile=certifi.where(), server_api=ServerApi('1'))
 
 # Send a ping to confirm a successful connection
 try:
